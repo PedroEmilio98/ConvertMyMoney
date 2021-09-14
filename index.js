9a00c0d6b5c9d9f31bc37cb5c5e15498cd1d91c5
Pedro Emilio <pedroemiliokuhn@gmail.com>
2021-09-14 16:08:38 -0300

    conversao e inicio dos testes

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const path = require('path')
+const convert = require('./lib/convert')
+
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-    res.render('home')
-})
 
 app.listen(3000, err => {
     if (err) {
@@ -17,4 +15,29 @@ app.listen(3000, err => {
         console.log('servidor no ar')
     }
 })
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/conversao', (req, res) => {
+    const { cotacao, dolares } = req.query
+    resultado = convert.convert(cotacao, dolares)
+    if (cotacao > 0 && dolares > 0) {
+        res.render('conversao', {
+            error: false,
+            resultado: convert.toMoney(resultado),
+            cotacao: convert.toMoney(cotacao),
+            dolares: convert.toMoney(dolares)
+        })
+    } else {
+        res.render('conversao', {
+            error: 'resultado inválido',
+        })
+    }
+})
+
+
+
 
